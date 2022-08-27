1a638546d5725a283a5444ea3c2863e392a042d9
ChayNhungKhongKhet <pkyoulike@gmail.com>
2022-08-27 23:13:25 +0700

    basic sketch

---
+++ app/src/main/java/com/phamtantb24/login_profile/Profile.java
@@ -0,0 +1,14 @@
+package com.phamtantb24.login_profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
